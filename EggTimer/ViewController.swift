10ccf8a0443fb674e8e471de621e5c1c9ab33eed
GW <gwsounddsg@gmail.com>
2020-08-07 10:52:08 -0700

    implements timer method to count down based on egg type selected, adds and implements progress bar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let eggTime: [String: Float] = ["Soft": 3.0, "Medium": 4.0, "Hard": 7.0]
+    var secondsRemaining: Float = 0.0
+    var timer = Timer()
+    var selectedCookTime: Float = 0.0
+    
+    @IBOutlet var label: UILabel!
+    @IBOutlet var progressBar: UIProgressView!
+    
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        guard let time = eggTime[sender.currentTitle!] else {return}
+        
+        selectedCookTime = time
+        secondsRemaining = time
+        label.text = "Cooking..."
+        
+        progressBar.progress = 0.0
+        progressBar.isHidden = false
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        secondsRemaining -= 1
+        progressBar.progress = (selectedCookTime - secondsRemaining) / selectedCookTime
+        
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds")
+        }
+        else {
+            label.text = "Done!"
+            timer.invalidate()
+        }
+    }
 }
